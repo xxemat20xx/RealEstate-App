00a18600f3e7efa7c86ca20c9dcf1ac65be734e7
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2026-03-04 11:38:06 +0800

    connect frontend to render backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5001/api/",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
